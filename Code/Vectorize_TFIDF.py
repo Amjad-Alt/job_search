03f9fe5bdc4d728543f8f16ed14c90bae8125d91
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-06 20:57:38 -0500

    Update Vectorize_TFIDF.py
    change variables names go with the flow of the previous script

--- Code/Vectorize_TFIDF.py
+++ Code/Vectorize_TFIDF.py
@@ -4,14 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # job discription 
 # for accounting assistant
-"""Reconcile invoices and identify discrepancies. Create and update expense reports.Process reimbursement forms.
+job_description ="""Reconcile invoices and identify discrepancies. Create and update expense reports.Process reimbursement forms.
 Prepare bank deposits. Enter financial transactions into internal databases. Check spreadsheets for accuracy.
 Maintain digital and physical financial records."""
 
 # Combine all texts for vectorization 
 # it is a must to combine then vectorize then sperate
 
-combined_texts = df['Cleaned_Resume'].tolist() + [job_description]
+combined_texts = resumes_with_id["Cleaned_Resume"].tolist() + [job_description]
 #%%
 
 # Vectorization
@@ -19,6 +19,6 @@ vectorizer = TfidfVectorizer()
 vectorized_texts = vectorizer.fit_transform(combined_texts)
 #%%
 # Splitting the vectorized texts back into resumes and job descriptions
-resumes_vec = vectorized_texts[:len(df)]
-job_desc_vec = vectorized_texts[len(df):]
+resumes_vec = vectorized_texts[:len(resumes_with_id)]
+job_desc_vec = vectorized_texts[len(resumes_with_id):]
 
