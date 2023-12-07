ea2d09e671fafcb95de16387e222ea2e1d886dc8
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-06 20:40:53 -0500

    Update clean_resumes.py
    add the ID column

--- Code/clean_resumes.py
+++ Code/clean_resumes.py
@@ -1,51 +1,49 @@
 
-
+import pandas as pd
+import nltk
 import re
 import string
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# Read the CSV file
 df = pd.read_csv("resumes_data.csv")
 
-# display the first few rows of the DataFrame
+# Display the first few rows of the DataFrame
+print("Original DataFrame:")
 print(df.head())
-# %%
-# Select the 'Resume' column
-resumes = df['Resume']
-print(resumes.head())
 
-# %%
-# clean
-# %%
+# Extract 'ID' and 'Resume' columns
+resumes_with_id = df[['ID', 'Resume']]
+
 # Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
-# %%
-# Initialize lemmatizer
+
+# Initialize the lemmatizer
 lemmatizer = WordNetLemmatizer()
 
 
 def clean_text(text):
-    # Lowercase
+    # Lowercase, remove line breaks, extra white spaces, and punctuation
     text = text.lower()
-    # Remove line breaks and extra white spaces
     text = re.sub(r'\n+', ' ', text)
     text = re.sub(r'\s+', ' ', text)
-    # Remove punctuation
     text = text.translate(str.maketrans('', '', string.punctuation))
-    # Tokenize
+
+    # Tokenize, remove stopwords, and lemmatize
     tokens = word_tokenize(text)
-    # Remove stopwords and lemmatize
     cleaned_text = [lemmatizer.lemmatize(
         token) for token in tokens if token not in stopwords.words('english')]
     return ' '.join(cleaned_text)
 
 
 # Apply cleaning to each resume
-cleaned_resumes = resumes.apply(clean_text)
+resumes_with_id['Cleaned_Resume'] = resumes_with_id['Resume'].apply(clean_text)
 
-# Print first few cleaned resumes
-print(cleaned_resumes.head())
-# %%
+# Print first few cleaned resumes with IDs
+print("Cleaned Resumes with IDs:")
+print(resumes_with_id[['ID', 'Cleaned_Resume']].head())
+
