cb633c41be39eda81c5ed4ca36e37a044c90e7f5
nammin <min.w82@gmail.com>
2023-12-08 15:14:00 -0500

    Updatea 12/8
    1. Updated codes (Modeling, util,etc)
    2. Made Sementic_similariy_Custom_Team2.py
    2. Upload classification model result data (predict jobzone on resume data)

--- Code/Classify_Job_zone_Team2.py
+++ Code/Classify_Job_zone_Team2.py
@@ -11,38 +11,64 @@ sys.path.insert(0, os.getcwd())
 from Utils_Team2 import *  # Call functions as Utils
 #os.listdir(os.getcwd())
 from datasets import load_dataset,Dataset,DatasetDict
+from transformers import DataCollatorForLanguageModeling
 from transformers import DataCollatorWithPadding,AutoModelForSequenceClassification, Trainer, TrainingArguments,AutoTokenizer,AutoModel,AutoConfig
 from transformers.modeling_outputs import TokenClassifierOutput
 import torch
 import torch.nn as nn
 import pandas as pd
+from torch.utils.data import DataLoader
+import numpy as np
+#%%
+################################################
+# [Classification] Model to Predict Job zone (with Job Corpus)
+# Model 1. Fine-tuning  roberta-based Model
+# huggingface (twitter-roberta-base-emotion)
+################################################
+#%%
+###############################################
+# [Outline of Fine-tuning- Customized Network]
+#    A. Inpt data Preprocessing (Use origin Tokenizer)
+#    B. ★ Build CustomModel
+#       - Extract BODY + Add New Head
+#       : Add Dropout Layer, Dense layer for 5 categories
+#    C. Train with Customized module
+###############################################
 #%%
 ################################################
 # Load file (Option 1. Use Github(URL), 2. Use Cloud directory,
 ################################################
-# 1. Job (Pickle, 2. Use Cloud directory)
-path = '/home/ubuntu/Project/Data_cleaned'
-df_job = pd.read_pickle(os.path.join(path, 'df_Occupation.pkl'))
-print(df_job.shape)
-#%%
-df_job.to_csv('./df_Occupation.csv')
-#%%
-# 2. Resume_previous one (2. Use Cloud directory)
-df_resume = pd.read_pickle(os.path.join(path, 'resume_data_cleaned.pkl'))
-print(df_resume.shape)
-#%%
-# Definition of Job zone
-df_Job_Zone_Ref = read(os.path.join('/home/ubuntu/Project/db_28_0_text/','Job Zone Reference.txt'))
-# ['Job Zone', 'Name', 'Experience', 'Education', 'Job Training', 'Examples', 'SVP Range']
-print(df_Job_Zone_Ref[['Job Zone', 'Name']].to_string()) # Job zone 1~5 (easy ~ difficult)
-#%%
+# 1. Job (CSV 1. Use Github(URL))
 url = r'https://raw.githubusercontent.com/Amjad-Alt/job_search/Nammin-Woo/Data_cleaned/df_Occupation.csv'
 df_job = pd.read_csv(url)
+
+# 2. Resume (New) (1. Use Github(URL))
+url = r'https://raw.githubusercontent.com/Amjad-Alt/job_search/Amjad/Code/resumes_data.csv'
+df_resume = pd.read_csv(url)
+#init_chk_df_2(df_resume)  #['ID', 'Resume', 'Category']
+
+# 2.Use Cloud directory)
+# path = '/home/ubuntu/Project/Data_cleaned'
+# df_job = pd.read_pickle(os.path.join(path, 'df_Occupation.pkl'))
+# df_resume = pd.read_pickle(os.path.join(path, 'df_New_resume_job_zone_pred.pkl')) # with job-zone predict
+# #old resume data(moe's):resume_data_cleaned
+
 #%%
-df_job.columns.to_list()
+# Definition of Job zone
+# df_Job_Zone_Ref = read(os.path.join('/home/ubuntu/Project/db_28_0_text/','Job Zone Reference.txt'))
+# # ['Job Zone', 'Name', 'Experience', 'Education', 'Job Training', 'Examples', 'SVP Range']
+# print(df_Job_Zone_Ref[['Job Zone', 'Name']].to_string()) # Job zone 1~5 (easy ~ difficult)
+
+#%%
+################################################
+# Preprocessing
+################################################
 #%%
 def create_zone_model_data():
-    # Load Preprocessed Job data and preprocess it for modeling
+    ''''
+    # Create data to make Classification Model (Ready to Load by Transformer)
+    '''
+    # Load current data (cleaned ONET JOB Corpus)
     url = r'https://raw.githubusercontent.com/Amjad-Alt/job_search/Nammin-Woo/Data_cleaned/df_Occupation.csv'
     df_job = pd.read_csv(url)
     # Explore Target # ['O*NET-SOC Code',  'Description_Job', 'Job Zone']
@@ -53,77 +79,57 @@ def create_zone_model_data():
     # df_job[df_job['Job Zone'].isnull()==1].Title
     df_job.dropna(subset=['Job Zone'], inplace=True)
     # df_job['Job Zone'].isnull().sum()
+    df_job['Job Zone'] = df_job.loc[:, 'Job Zone'].astype('int64')  #change label type to INT
     print("After Cleansing NaN Label")
+    df_job['Job Zone'] = df_job['Job Zone'] -1  #Align to Transformer (label should start from 0)
     print(df_job['Job Zone'].value_counts(normalize=True, dropna=False).sort_index())
     df_job.rename(columns={'Job Zone': 'label'}, inplace=True)
     df_job = df_job[['Description_Job', 'label']]
     return df_job
 #%%
-df = create_zone_model_data()
+create_zone_model_data().to_csv("./job_zone_model_data.csv") # to_csv, for load_dataset()
 #%%
-df_job = df_job.to_csv("./data.csv")
-
-
-
-#%%
+# Transformer format: DatasetDict
+data=load_dataset("csv",data_files="./job_zone_model_data.csv")
 
 #%%
+def prep_zone_model_train_data(data):
+    '''Preprocessing of Transformer data'''
+    data = data.remove_columns(['Unnamed: 0'])
+    data.set_format('pandas')
+    data = data['train'][:]
+    data.drop_duplicates(subset=['Description_Job'], inplace=True)
+    data = data.reset_index()[['Description_Job', 'label']]
+    data = Dataset.from_pandas(data)
+    return data
 
 #%%
-# Convert into Transformer dataset
-import copy
-data = copy.deepcopy(df_job)
-data.rename(columns={'Job Zone': 'label'},inplace=True)
-data=data[['Description_Job','label']]
-data.to_csv("./data.csv")
+data = prep_zone_model_train_data(data) #923, train:
 #%%
-
-
+# Split train, evaluation dataset (70%, 30%)
+def train_test_split(data, test_size):
+    # train(70%), test(30%)
+    train_test = data.train_test_split(test_size=test_size, seed=15)
+    # gather as a single DatasetDict
+    data = DatasetDict({
+        'train': train_test['train'],
+        'test': train_test['test']})
+    return data
 #%%
-data.set_format('pandas')
-data=data['train'][:]
-
-data.drop_duplicates(subset=['headline'],inplace=True)
-data=data.reset_index()[['headline','label']]
-data=Dataset.from_pandas(data)
+data = train_test_split(data,0.3)
 
 #%%
-data=load_dataset("csv",data_files="./Sarcasm_Headlines_Dataset_v2.json")
+################################################
+# 1. Define Training Module
+################################################
 #%%
-data  #28503
-#%%
-# 80% train, 20% (10% test + 10% validation)
-train_testvalid = data.train_test_split(test_size=0.2,seed=15)  #train, test(20%: disgard)
-# Split the 10% test + valid in half test, half valid
-test_valid = train_testvalid['test'].train_test_split(test_size=0.5,seed=15)  #train:valid, test: test
-
-#%%
-# gather everyone if you want to have a single DatasetDict
-data = DatasetDict({
-    'train': train_testvalid['train'],
-    'test': test_valid['test'],
-    'valid': test_valid['train']})
-data
-#%%
-data['train'][0]
-#%%
-###############################################
-# [Train with Customized model]
-#    A. Inpt data Preprocessing (Use origin Tokenizer)
-#    B. ★ Build CustomModel
-#       - Extract BODY
-#       - Add New Head
-#         : Add Dropout Layer, Extract embedded output, Dense layer for 2 categories
-#    C. Train with Customized module
-###############################################
-
-#%%
+# Define pretrained model
 checkpoint = "cardiffnlp/twitter-roberta-base-emotion"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 tokenizer.model_max_len=512
 #%%
 def tokenize(batch):
-  return tokenizer(batch["headline"], truncation=True,max_length=512)
+  return tokenizer(batch["Description_Job"], truncation=True,max_length=512)
 
 tokenized_dataset = data.map(tokenize, batched=True)
 tokenized_dataset
@@ -131,10 +137,12 @@ tokenized_dataset
 tokenized_dataset['train'][0]
 #%%
 tokenized_dataset.set_format("torch",columns=["input_ids", "attention_mask", "label"])
+#data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
 #Option: used in BERT(language modeling and applies random masking)
 #data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
+#%%
+tokenized_dataset['train'][0] #need to check torch= tensor
 #%%
 class CustomModel(nn.Module):
     def __init__(self, checkpoint, num_labels):
@@ -175,17 +183,17 @@ class CustomModel(nn.Module):
 #%%
 # GPU(device), Define Model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model=CustomModel(checkpoint=checkpoint,num_labels=2).to(device)
+model=CustomModel(checkpoint=checkpoint,num_labels=5).to(device)
 #%%
 #  (Define) Dataloader
-batch_size = 32
+batch_size = 16
 from torch.utils.data import DataLoader
 
 train_dataloader = DataLoader(
     tokenized_dataset["train"], shuffle=True, batch_size=batch_size, collate_fn=data_collator
 )
 eval_dataloader = DataLoader(
-    tokenized_dataset["valid"], batch_size=batch_size, collate_fn=data_collator
+    tokenized_dataset["test"], batch_size=batch_size, collate_fn=data_collator
 )
 #%%
 # Set Hyperparameters,
@@ -206,9 +214,15 @@ print(num_training_steps)
 #%%
 # Define Evaluate metric (classification)
 from datasets import load_metric
-metric = load_metric("f1")
+metric = load_metric("accuracy") #multi class
+#metric = load_metric("f1")
 
 #%%
+################################################
+# 2. Training and Evaluate (execute modules)
+################################################
+#%%
+
 # (Define) Training(w train_dataloader)/Evaluate(w test_dataloader)/Training loop (Epoch)
 from tqdm.auto import tqdm
 
@@ -241,26 +255,117 @@ for epoch in range(num_epochs):
 
     print(metric.compute())  # check performance on validation set
 
-#%%
-# Check final performance on test set
-model.eval()
-
-test_dataloader = DataLoader(
-    tokenized_dataset["test"], batch_size=32, collate_fn=data_collator
-)
-
-for batch in test_dataloader:
-    batch = {k: v.to(device) for k, v in batch.items()}
-    with torch.no_grad():
-        outputs = model(**batch)
-
-    logits = outputs.logits
-    predictions = torch.argmax(logits, dim=-1)
-    metric.add_batch(predictions=predictions, references=batch["labels"])
-
-metric.compute()
-
 #%%M
-model_path = "./Model_Nammin_Woo.pt"  # specify the path to save the model
-torch.save(model.state_dict(), model_path)
+# Save Pytorch Model
+# Model 1 : {'accuracy': 0.6137184115523465}
+model_path = "./Model_Classify_Job_zone.pt"  # specify the path to save the model
+#torch.save(model.state_dict(), model_path)
+#model.load_state_dict(torch.load(model_path))
+
 #%%
+################################################
+# 3. Job Zone classify model Test on Resume Data (for application)
+################################################
+#%%
+def create_zone_model_test_resume(): # 2481 resumes
+    url = r'https://raw.githubusercontent.com/Amjad-Alt/job_search/Amjad/Code/resumes_data.csv'
+    df_resume = pd.read_csv(url)  #2484
+    # Clean Null description
+    df_resume.dropna(subset=['Resume'], inplace=True)  # 2483
+    df_resume = df_resume.drop_duplicates(subset=['Resume']) # 2481
+    return df_resume # Keep df_resume (for analysis/application later)
+#%%
+df_resume = create_zone_model_test_resume()
+df_resume['Resume'].to_csv("./job_zone_model_resume_test.csv")
+# Transformer format: DatasetDict
+data=load_dataset("csv",data_files="./job_zone_model_resume_test.csv")
+#%%
+def prep_zone_model_test_resume(data):
+    '''Create test data same as training data (Preprocessing, tokenize)'''
+    data = data.remove_columns(['Unnamed: 0'])
+    data.set_format('pandas')
+    data = data['train'][:]
+    #data = data['train'][:50]  # Sample for test, # TBD: randomly choose sample in dataloader
+    data = Dataset.from_pandas(data)
+    return data
+#%%
+data = prep_zone_model_test_resume(data)  #2481
+#%%
+data
+#%%
+checkpoint = "cardiffnlp/twitter-roberta-base-emotion" # Pretrained model
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer.model_max_len=512
+#%%
+def tokenize_resume(batch):
+  return tokenizer(batch["Resume"], truncation=True,max_length=512)
+#%%
+tokenized_dataset = data.map(tokenize_resume, batched=True)
+#%%
+tokenized_dataset.set_format("torch",columns=["input_ids", "attention_mask"])
+#%%
+#tokenized_dataset[0]
+#tokenized_dataset.set_format("torch",columns=["input_ids", "attention_mask", "label"]) #Trainset
+#%%
+def predict(model, test_loader, device):
+    model.eval()  # Set the model to evaluation mode
+    predictions = []
+    for batch in test_loader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=-1)
+        # Flatten prediction tensor before appending to the list (ex. 40 with 3 batches: 16,16,8)
+        prediction = prediction.view(-1)
+        predictions.append(prediction)
+        # predictions.extend(pred.tolist())  # Convert tensor to list and extend
+        # predictions = np.array(predictions)  # Convert list to numpy array
+    predictions = torch.cat(predictions) #Concatenate the list of tensors into one tensor
+    predictions = predictions.tolist()
+    return predictions
+#%%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_path = "./Model_Classify_Job_zone.pt"  # Load Path of the Saved model 1
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model=CustomModel(checkpoint=checkpoint,num_labels=5).to(device) # CustomModel: Set trained model 1
+model.load_state_dict(torch.load(model_path))
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+# Resume Dataloader, batch_size 16(train), NO shuffle (not train)
+test_dataloader = DataLoader(tokenized_dataset, batch_size=16, shuffle=False, collate_fn=data_collator)
+#%%
+results = predict(model, test_dataloader, device)
+# Job zone : Label + 1 (Label was modified to align with Transformer (start from 0): label = job zone -1)
+results_ = [x + 1 for x in results]
+#%%
+print(len(results)) #2481
+#%%
+#np.save('zone_model_test_resume.npy', results) # array save (Bult results on Resume data)
+#results = np.load('zone_model_test_resume.npy')
+#%%
+# Freq
+from collections import Counter
+print(Counter(results_))  # {4: 1707, 3: 555, 5: 214, 2: 5}
+
+# Job Zone                                            Name
+# 0         1   Job Zone One: Little or No Preparation Needed
+# 1         2           Job Zone Two: Some Preparation Needed
+# 2         3       Job Zone Three: Medium Preparation Needed
+# 3         4  Job Zone Four: Considerable Preparation Needed
+# 4         5     Job Zone Five: Extensive Preparation Needed
+#%%
+# Combine Predicted Job zone with an Original Resume data
+df_resume['Job_Zone_pred'] = results_
+df_resume.tail()
+#%%
+# # Distribution of elements' level values
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# sns.set_theme(style="darkgrid")
+# sns.displot(df_resume, x="Job_Zone_pred", kde = True)
+# plt.tight_layout()
+# plt.show()
+#%%
+# save_as_pickle(df_resume, '/home/ubuntu/Project/Data_cleaned', 'df_New_resume_job_zone_pred.pkl') # Check Point
+# df_resume.to_csv("./resumes_data_zone_pred.csv")
