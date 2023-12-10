c67b8b835cba0dea5605b2a30aede2b20ef2c93e
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2023-12-09 20:26:08 -0500

    Update job_zone_NLP.py
    fixed bugs and did grid search then saved model

--- Code/job_zone_NLP.py
+++ Code/job_zone_NLP.py
@@ -1,188 +1,174 @@
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
-from torch.utils.data import DataLoader
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW,  get_scheduler
-import torch.nn as nn
-from transformers import BertModel, AutoConfig, AutoModel
-import torch
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW, AutoTokenizer
+
+import os
+import sys
 import pandas as pd
 import numpy as np
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, MWETokenizer
-from nltk.collocations import BigramCollocationFinder, BigramAssocMeasures
-from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import itertools
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from datasets import load_dataset, DatasetDict, load_metric
+from transformers import (AutoTokenizer, AutoModelForSequenceClassification, 
+                          AutoConfig, DataCollatorWithPadding, AdamW, get_scheduler)
+from tqdm.auto import tqdm
+from sklearn.metrics import accuracy_score
+sys.path.insert(0, os.getcwd())
+from Utils_Team2 import *
 
-# Download necessary NLTK data (to be executed if needed)
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download('stopwords')
-# Load the CSV file
-df_occupation = pd.read_csv("df_Occupation.csv")
+# Load and preprocess data
+def create_zone_model_data():
+    url = 'https://raw.githubusercontent.com/Amjad-Alt/job_search/Nammin-Woo/Data_cleaned/df_Occupation.csv'
+    df_job = pd.read_csv(url)
+    df_job.dropna(subset=['Job Zone'], inplace=True)
+    
+    # Concatenate the text columns
+    text_columns = ['Description', 'Description_Abilities', 'Description_Knowledge', 'Description_Skills']
+    df_job['combined_text'] = df_job[text_columns].fillna('').agg(' '.join, axis=1)
 
-# Combining text columns into a single column
-text_columns = ['Description', 'Description_Abilities',
-                'Description_Knowledge', 'Description_Skills']
-df_occupation['combined_text'] = df_occupation[text_columns].fillna(
-    '').agg(' '.join, axis=1)
+    df_job['Job Zone'] = df_job['Job Zone'].astype('int64') - 1
+    df_job.rename(columns={'Job Zone': 'label'}, inplace=True)
+    df_job = df_job[['combined_text', 'label']]
+    return df_job
 
-# Handle missing values
-df_occupation['Title'].fillna('NA', inplace=True)
-df_occupation['combined_text'].fillna('NA', inplace=True)
-df_occupation = df_occupation[(df_occupation['Title'] != 'NA') & (
-    df_occupation['combined_text'] != 'NA')]
+df_job = create_zone_model_data()
+df_job.to_csv("./job_zone_model_data.csv")
 
-# Split the data for model training (optional, depending on further use)
-X = df_occupation['combined_text']
-y = df_occupation['Job Zone_x']
+# Load dataset
+data = load_dataset("csv", data_files="./job_zone_model_data.csv")
 
-# Split data into training and testing sets
-train_texts, test_texts, train_labels, test_labels = train_test_split(
-    X, y, test_size=0.2, random_state=42)
-# %%
+# Preprocess dataset
+def preprocess_data(data):
+    '''Preprocessing of Transformer data'''
+    # Convert the train dataset to a Pandas DataFrame
+    df = data['train'].to_pandas()
+
+    # Apply DataFrame operations
+    df = df.drop_duplicates(subset=['combined_text'])
+    df = df.reset_index(drop=True)[['combined_text', 'label']]
+
+    # Convert back to Hugging Face dataset
+    return Dataset.from_pandas(df)
+
+data = preprocess_data(data)
+data = data.train_test_split(test_size=0.3, seed=15)
 
 # Tokenization
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer.model_max_len = 512
 
+def tokenize(batch):
+    return tokenizer(batch["combined_text"], truncation=True, max_length=512)
 
-def tokenize_function(texts):
-    return tokenizer(texts, truncation=True, padding='max_length', max_length=570)
+tokenized_dataset = data.map(tokenize, batched=True)
+tokenized_dataset.set_format("torch", columns=["input_ids", "attention_mask", "label"])
 
-
-# Apply tokenization to the texts
-train_encodings = tokenize_function(train_texts.tolist())
-test_encodings = tokenize_function(test_texts.tolist())
-
-
-class CustomDataset(Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx])
-                for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels)
-
-
-train_dataset = CustomDataset(train_encodings, train_labels)
-test_dataset = CustomDataset(test_encodings, test_labels)
-
-# DataLoader
-train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-
-
-# %%
-
-# Model
-model_name = "bert-base-uncased"
-config = AutoConfig.from_pretrained(model_name)
-pretrained_model = AutoModel.from_pretrained(model_name, config=config)
-
-
+# Define Classifier
 class Classifier(nn.Module):
     def __init__(self, pretrained_model, num_labels):
         super(Classifier, self).__init__()
         self.pretrained_model = pretrained_model
-        self.classifier = nn.Linear(config.hidden_size, num_labels)
+        self.classifier = nn.Linear(pretrained_model.config.hidden_size, num_labels)
 
-    def forward(self, input_ids, attention_mask):
-        outputs = self.pretrained_model(
-            input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = outputs.pooler_output
-        logits = self.classifier(pooled_output)
-        return logits
+    def forward(self, input_ids, attention_mask=None):
+        outputs = self.pretrained_model(input_ids=input_ids, attention_mask=attention_mask)
+        return self.classifier(outputs.pooler_output)
 
+# Model setup
+num_labels = 5
+model = Classifier(AutoModel.from_pretrained(checkpoint, config=AutoConfig.from_pretrained(checkpoint)), num_labels).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
-num_labels = len(np.unique(y))
-model = Classifier(pretrained_model, num_labels)
+# Dataloader
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+train_dataloader = DataLoader(tokenized_dataset["train"], shuffle=True, batch_size=16, collate_fn=data_collator)
+eval_dataloader = DataLoader(tokenized_dataset["test"], batch_size=16, collate_fn=data_collator)
 
-# %%
-# Hyperparameters for grid search
-learning_rates = [1e-5, 3e-5, 5e-5]
-batch_sizes = [16, 32]
+# Training setup
 num_epochs = 3
-# Define a function for model training and evaluation
+optimizer = AdamW(model.parameters(), lr=5e-5)
+lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_epochs * len(train_dataloader))
 
+# Define device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_and_evaluate(model, train_dataloader, test_dataloader, learning_rate, criterion, lr_scheduler):
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
-    device = torch.device(
-        "cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
-
+# Function to train and evaluate model
+def train_and_evaluate(model, train_dataloader, eval_dataloader, optimizer, lr_scheduler, device, num_epochs):
     # Training loop
     model.train()
-    for batch in train_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(input_ids=batch['input_ids'],
-                        attention_mask=batch['attention_mask'])
-        loss = criterion(outputs, batch['labels'].long())
+    for epoch in range(num_epochs):
+        for batch in train_dataloader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        lr_scheduler.step()
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            loss_function = nn.CrossEntropyLoss()
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
 
     # Evaluation loop
-    model.eval()
     total_correct = 0
-    total_samples = 0
-    with torch.no_grad():
-        for batch in test_dataloader:
-            batch = {k: v.to(device) for k, v in batch.items()}
-            outputs = model(
-                input_ids=batch['input_ids'], attention_mask=batch['attention_mask'])
-            predictions = outputs.argmax(dim=1)
-            total_correct += (predictions == batch['labels']).sum().item()
-            total_samples += batch['labels'].size(0)
+    total_predictions = 0
+    model.eval()
+    for batch in eval_dataloader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
 
-    # Calculate and return total accuracy
-    total_accuracy = total_correct / total_samples
-    return total_accuracy
+        with torch.no_grad():
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
 
-# %%
+        logits = outputs
+        predictions = torch.argmax(logits, dim=-1)
+        total_correct += (predictions == labels).sum().item()
+        total_predictions += labels.size(0)
 
+    # Calculate accuracy
+    return total_correct / total_predictions
+
+# Hyperparameters for grid search
+learning_rates = [1e-5, 3e-5, 5e-5]
+batch_sizes = [8, 16, 32]
+num_epochs_options = [3, 4]
 
 # Grid search
 best_accuracy = 0
 best_hyperparameters = {}
 
-for lr, batch_size in itertools.product(learning_rates, batch_sizes):
-    # Initialize dataloaders with the current batch size
-    train_dataloader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=False)
+for lr, batch_size, num_epochs in itertools.product(learning_rates, batch_sizes, num_epochs_options):
+    # DataLoaders for current batch size
+    train_dataloader = DataLoader(tokenized_dataset["train"], shuffle=True, batch_size=batch_size, collate_fn=DataCollatorWithPadding(tokenizer))
+    eval_dataloader = DataLoader(tokenized_dataset["test"], batch_size=batch_size, collate_fn=DataCollatorWithPadding(tokenizer))
 
-    # Initialize the model
-    model = Classifier(pretrained_model, num_labels)
-
-    # Set up criterion and scheduler
-    criterion = nn.CrossEntropyLoss()
-    num_training_steps = 3 * len(train_dataloader)  # Assuming 3 epochs
-    lr_scheduler = get_scheduler("linear", optimizer=AdamW(model.parameters(), lr=lr),
-                                 num_warmup_steps=0, num_training_steps=num_training_steps)
+    # Initialize model for current hyperparameters
+    bert_model = AutoModel.from_pretrained(checkpoint, config=AutoConfig.from_pretrained(checkpoint))
+    model = Classifier(bert_model, num_labels).to(device)
+    optimizer = AdamW(model.parameters(), lr=lr)
+    lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_epochs * len(train_dataloader))
 
     # Train and evaluate the model
-    accuracy = train_and_evaluate(
-        model, train_dataloader, test_dataloader, lr, criterion, lr_scheduler)
+    accuracy = train_and_evaluate(model, train_dataloader, eval_dataloader, optimizer, lr_scheduler, device, num_epochs)
 
-    # Update best model if current model is better
+    # Check if current model is the best one
     if accuracy > best_accuracy:
         best_accuracy = accuracy
-        best_hyperparameters = {'learning_rate': lr, 'batch_size': batch_size}
+        best_hyperparameters = {'learning_rate': lr, 'batch_size': batch_size, 'num_epochs': num_epochs}
 
-print("Best Hyperparameters:", best_hyperparameters)
+# Print the best hyperparameters and their corresponding accuracy
+print(f"Best Hyperparameters: {best_hyperparameters}, Best Accuracy: {best_accuracy:.4f}")
+
+# Define the path to save the model
+model_save_path = "./trained_model2.pth"
+
+# Save the model's state dictionary
+torch.save(model.state_dict(), model_save_path)
+
+# Load the model
+def load_model(model_path, pretrained_model, num_labels):
+    model = Classifier(pretrained_model, num_labels)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()  # Set the model to evaluation mode
+    return model
